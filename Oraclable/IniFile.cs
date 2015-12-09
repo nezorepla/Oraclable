ed93ac8ee23a363eb494d52eeb041bc9111259f2
nezorepla <alperozen84@gmail.com>
2015-12-09 23:09:14 +0200

    init

---
+++ Oraclable/IniFile.cs
@@ -0,0 +1,66 @@
+﻿ 
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+namespace Oraclable
+{
+   
+
+// Change this to match your program's normal namespace
+namespace MyProg
+{
+    class IniFile   // revision 10
+    {
+        string Path;
+        string EXE = Assembly.GetExecutingAssembly().GetName().Name;
+
+        [DllImport("kernel32")]
+        static extern long WritePrivateProfileString(string Section, string Key, string Value, string FilePath);
+
+        [DllImport("kernel32")]
+        static extern int GetPrivateProfileString(string Section, string Key, string Default, StringBuilder RetVal, int Size, string FilePath);
+
+        public IniFile(string IniPath )
+        {
+            Path = new FileInfo(IniPath ?? EXE + ".ini").FullName.ToString();
+        }
+
+        public string Read(string Key, string Section )
+        {
+            var RetVal = new StringBuilder(255);
+            GetPrivateProfileString(Section ?? EXE, Key, "", RetVal, 255, Path);
+            return RetVal.ToString();
+        }
+
+        public void Write(string Key, string Value, string Section)
+        {
+            WritePrivateProfileString(Section ?? EXE, Key, Value, Path);
+        }
+        public void Write(string Key, string Value)
+        {
+            WritePrivateProfileString(null ?? EXE, Key, Value, Path);
+        }
+
+        public void DeleteKey(string Key, string Section )
+        {
+            Write(Key, null, Section ?? EXE);
+        }
+
+        public void DeleteSection(string Section )
+        {
+            Write(null, null, Section ?? EXE);
+        }
+
+        public bool KeyExists(string Key, string Section )
+        {
+            return Read(Key, Section).Length > 0;
+        }
+
+        //internal void Write(string p, string p_2)
+        //{
+        //    throw new System.NotImplementedException();
+        //}
+    }
+}
+}
---
+++ Oraclable/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.IO;
+using Oraclable.MyProg;
+
+namespace Oraclable
+{
+       class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Creates or loads an INI file in the same directory as your executable
+            // named EXE.ini (where EXE is the name of your executable)
+           // var MyIni = new IniFile();
+
+            // Or specify a specific name in the current dir
+            //var MyIni = new IniFile("Settings.ini");
+
+            // Or specify a specific name in a specific dir
+            var MyIni = new IniFile(@"C:\Users\Alper\Documents\GitHub\Oraclable\Settings.ini");
+
+            MyIni.Write("DefaultVolume", "100");
+            MyIni.Write("HomePage", "http://www.google.com");
+        }
+    
+    
+
+    }
+}
